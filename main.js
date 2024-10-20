4c21413e55a3c01636b18f863737da3355da56fe
MaRK0960 <ahmedhesham0960@outlook.com>
2024-10-20 22:34:52 +0300

    Prepare for live:
    * Use https
    * Render content instead of console log

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 window.onload = async () => {
 
     const forcast =
-        await fetch("http://api.weatherapi.com/v1/forecast.json?key=ce9fa4640f164e42a61200442241910&q=Ismailia&days=1&aqi=no&alerts=no")
+        await fetch("https://api.weatherapi.com/v1/forecast.json?key=ce9fa4640f164e42a61200442241910&q=Ismailia&days=1&aqi=no&alerts=no")
             .then(res => res.json())
             .then(res => {
                 const day = res.forecast.forecastday[0].day;
                 return { min: day.mintemp_c, max: day.maxtemp_c }
             });
 
-    console.log(forcast);
+    const forcastElement = document.createElement("h3");
+    forcastElement.append(`Min: ${forcast.min}\tMax: ${forcast.max}`);
+
+    document.body.append(forcastElement);
 
 };
